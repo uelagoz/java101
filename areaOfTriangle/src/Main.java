580d68e3a38d4d76c5578bb595b4f9249da98daf
uelagoz <u.elagozz@gmail.com>
2023-07-07 16:06:25 +0300

    areaOfTriangle added.

---
+++ areaOfTriangle/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        double a, b, c, u, area;
+
+        System.out.println("Üçgenin 1.kenar uzunluğunu giriniz: ");
+        a = scanner.nextInt();
+        System.out.println("Üçgenin 2.kenar uzunluğunu giriniz: ");
+        b = scanner.nextInt();
+        System.out.println("Üçgenin 3.kenar uzunluğunu giriniz: ");
+        c = scanner.nextInt();
+
+
+        u = (a + b + c) / 2;
+        area = Math.sqrt(u * (u - a) * (u - b) * (u - c));
+        System.out.println(area);
+    }
+}
