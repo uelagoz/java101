3a801ee3d2352682fd41803f0e413afbe23a4bdd
uelagoz <u.elagozz@gmail.com>
2023-07-07 15:32:22 +0300

    taxCalculator added.

--- taxCalculator/src/Main.java
+++ taxCalculator/src/Main.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Lütfen KDV'siz fiyatı giriniz: ");
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+
+        if ( input <1000){
+            double taxAmount = input * 0.18;
+            double total = input + (input * 0.18);
+            System.out.println("KDV tutarı: " + taxAmount);
+            System.out.println("KDV'li fiyat: " + total);
+        }else {
+            double taxAmount = input * 0.08;
+            double total = input + (input * 0.08);
+            System.out.println("KDV tutarı: " + taxAmount);
+            System.out.println("KDV'li fiyat: " + total);
+        }
     }
 }
