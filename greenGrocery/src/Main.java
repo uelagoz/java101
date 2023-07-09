5a115fbb3b20a286545309c8918d6c95911940ae
uelagoz <u.elagozz@gmail.com>
2023-07-09 11:58:18 +0300

    Main.java updated.

--- greenGrocery/src/Main.java
+++ greenGrocery/src/Main.java
@@ -4,26 +4,22 @@ public class Main {
     public static void main(String[] args) {
         double pearPrice = 2.14, applePrice = 3.67, tomatoPrice = 1.11, bananaPrice = 0.95, eggplantPrice = 5.0;
         double pearKg, appleKg, tomatoKg, bananaKg, eggplantKg, totalCost;
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Kaç kilo Armut aldığınızı giriniz: ");
-        Scanner pearScanner = new Scanner(System.in);
-        pearKg = pearScanner.nextDouble();
+        pearKg = scanner.nextDouble();
 
         System.out.println("Kaç kilo Elma aldığınızı giriniz: ");
-        Scanner appleScanner = new Scanner(System.in);
-        appleKg = appleScanner.nextDouble();
+        appleKg = scanner.nextDouble();
 
         System.out.println("Kaç kilo Domates aldığınızı giriniz: ");
-        Scanner tomatoScanner = new Scanner(System.in);
-        tomatoKg = tomatoScanner.nextDouble();
+        tomatoKg = scanner.nextDouble();
 
         System.out.println("Kaç kilo Muz aldığınızı giriniz: ");
-        Scanner bananaScanner = new Scanner(System.in);
-        bananaKg = bananaScanner.nextDouble();
+        bananaKg = scanner.nextDouble();
 
         System.out.println("Kaç kilo Patlıcan aldığınızı giriniz: ");
-        Scanner eggplantScanner = new Scanner(System.in);
-        eggplantKg = eggplantScanner.nextDouble();
+        eggplantKg = scanner.nextDouble();
 
         totalCost = (pearKg * pearPrice) + (appleKg * applePrice) + (tomatoKg * tomatoPrice) + (bananaKg * bananaPrice) + (eggplantKg * eggplantPrice);
         System.out.println(totalCost);
