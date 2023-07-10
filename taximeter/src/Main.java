62b12ebb823a8fce117a9c70170e042f50c570d0
uelagoz <u.elagozz@gmail.com>
2023-07-10 11:10:37 +0300

    taximeter updated.

--- taximeter/src/Main.java
+++ taximeter/src/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         int openingFee = 10, min = 20;
         double km, totalFee, perKm = 2.20;
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Lütfen kaç km gitmek istediğinizi yazınız: ");
-        Scanner scanner = new Scanner(System.in);
         km = scanner.nextInt();
 
         totalFee = openingFee + (km * perKm);
