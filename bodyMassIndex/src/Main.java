071fc306e87b90f077bb969b9efc8782f8fcf9f5
uelagoz <u.elagozz@gmail.com>
2023-07-10 11:09:21 +0300

    bodyMassIndex updated.

--- bodyMassIndex/src/Main.java
+++ bodyMassIndex/src/Main.java
@@ -3,14 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         double weight, height, bodyMass;
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Lütfen kilonuzu giriniz: ");
-        Scanner scanner = new Scanner(System.in);
         weight = scanner.nextDouble();
 
         System.out.println("Lütfen boyunuzu(metre cinsinde) giriniz: ");
-        Scanner scanner2 = new Scanner(System.in);
-        height = scanner2.nextDouble();
+        height = scanner.nextDouble();
 
         bodyMass = weight / (height * height);
         System.out.println("Vücut Kitle İndeksiniz: " + bodyMass);
