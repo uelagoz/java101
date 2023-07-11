d0cc272521bc325956ac936682ac383de5808243
uelagoz <u.elagozz@gmail.com>
2023-07-11 11:37:23 +0300

    SortingNumbers added.

--- SortingNumbers/src/Main.java
+++ SortingNumbers/src/Main.java
@@ -16,27 +16,27 @@ public class Main {
 
         if (number1 > number2 && number1 > number3) {
             if (number2 > number3) {
-                System.out.println("number1 > number2 > number3");
-                System.out.println(number1 + " > " + number2 + " > " + number3);
+                System.out.println("number1>number2>number3");
+                System.out.println(number1 + ">" + number2 + ">" + number3);
             } else {
-                System.out.println("number1 > number3 > number2");
-                System.out.println(number1 + " > " + number3 + " > " + number2);
+                System.out.println("number1>number3>number2");
+                System.out.println(number1 + ">" + number3 + ">" + number2);
             }
         } else if (number2 > number1 && number2 > number3) {
             if (number1 > number3) {
-                System.out.println("number2 > number1 > number3");
-                System.out.println(number2 + " > " + number1 + " > " + number3);
+                System.out.println("number2>number1>number3");
+                System.out.println(number2 + ">" + number1 + ">" + number3);
             } else {
-                System.out.println("number2 > number3 > number1");
-                System.out.println(number2 + " > " + number3 + " > " + number1);
+                System.out.println("number2>number3>number1");
+                System.out.println(number2 + ">" + number3 + ">" + number1);
             }
         } else {
             if (number1 > number2) {
-                System.out.println("number3 > number1 > number2");
-                System.out.println(number3 + " > " + number1 + " > " + number2);
+                System.out.println("number3>number1>number2");
+                System.out.println(number3 + ">" + number1 + ">" + number2);
             } else {
-                System.out.println("number3 > number2 > number1");
-                System.out.println(number3 + " > " + number2 + " > " + number1);
+                System.out.println("number3>number2>number1");
+                System.out.println(number3 + ">" + number2 + ">" + number1);
             }
         }
     }
