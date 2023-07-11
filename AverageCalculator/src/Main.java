1ced6c38dc5778fa9619acb5de9b1a17dd7c97ed
uelagoz <u.elagozz@gmail.com>
2023-07-11 22:55:14 +0300

    AverageCalculator updated.

--- AverageCalculator/src/Main.java
+++ AverageCalculator/src/Main.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int number, sum = 0, count = 0;
+        int number, sum = 0, count = 0; //Toplamı saklamak ve bölünen sayıların adedini öğrenmek için değişken tanımlanması
         Scanner scanner = new Scanner(System.in);
 
+        // Kullanıcıdan gerekli bilgilerin alınması
         System.out.print("Bir sayı girin: ");
         number = scanner.nextInt();
 
@@ -12,12 +13,13 @@ public class Main {
             if (i % 3 == 0 && i % 4 == 0) {
                 sum += i;
                 count++;
-                System.out.println(i);
+                System.out.print("3 ve 4'e tam bölünen sayılar: " + i + " ");//Bölünen sayıların ekrana yazdırılması
             }
         }
 
+        // Sonuçların ekrana yazdırılması
         if (count != 0) {
-            double average = (double) sum / count;
+            double average = (double) sum / count; //Ortalamanın hesaplanması
             System.out.println("3 ve 4'e tam bölünen sayıların ortalaması: " + average);
         } else {
             System.out.println("3 ve 4'e tam bölünen sayı bulunamadı.");
